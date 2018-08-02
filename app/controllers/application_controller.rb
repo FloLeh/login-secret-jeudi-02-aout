7b45e79bfea5c0e896fb7ec939d37b660fc56dfb
Florian <florianlehuede@gmail.com>
2018-08-02 12:55:45 +0200

    logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :danger
+  include SessionsHelper
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
     else
       redirect_to login_path, danger: "Mauvais email ou password"
     end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
