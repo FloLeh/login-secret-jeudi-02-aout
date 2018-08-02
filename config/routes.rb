febd02e607dc50c7557dd1007741e496d2c548ed
Florian <florianlehuede@gmail.com>
2018-08-02 11:53:51 +0200

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
   get '/answer', to: 'static_pages#answer', as: "answer"
+
+  get    '/signup',  to: 'users#new', as: "signup"
+  get    '/login',   to: 'sessions#new', as: "login"
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
